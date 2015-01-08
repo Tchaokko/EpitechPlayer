256a1d5b32627a2c23f721ca68d5f0dbcfe6fd2e
lebars-r <romain.lebars@epitech.eu>
2015-01-08 17:29:39 +0100

    Library

---
+++ WpfApplication1/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+
+        }
+
+        private void Button_Musics(object sender, RoutedEventArgs e)
+        {
+            string path;
+            path = Environment.GetEnvironmentVariable("USERPROFILE") + "\\Music";
+            Console.WriteLine(path);
+            DirectoryInfo info = new DirectoryInfo(path);
+            if (info.Exists)
+            {
+                myTree.ItemsSource = info.GetFiles();
+            }
+        }
+
+        private void Button_Videos(object sender, RoutedEventArgs e)
+        {
+            string path;
+            path = Environment.GetEnvironmentVariable("USERPROFILE") + "\\Videos";
+            Console.WriteLine(path);
+            DirectoryInfo info = new DirectoryInfo(path);
+            if (info.Exists)
+            {
+                myTree.ItemsSource = info.GetFiles();
+            }
+        }
+
+        private void Button_Image(object sender, RoutedEventArgs e)
+        {
+            string path;
+            path = Environment.GetEnvironmentVariable("USERPROFILE") + "\\Pictures";
+            Console.WriteLine(path);
+            DirectoryInfo info = new DirectoryInfo(path);
+            if (info.Exists)
+            {
+                myTree.ItemsSource = info.GetFiles();
+            }
+        }
+
+        private void MouseDoubleClickMyTree(object sender, MouseButtonEventArgs e)
+        {
+            Console.WriteLine(myTree.ToString());
+        }
+    }
+}
